3f6e217e6ed7545b7d133adf46f0fcb1f8819cc7
Kevin Bell <bellKevin@pm.me>
2022-09-21 16:33:48 +0000

    Add files via upload

---
+++ studentScoresInFile/src/studentscoresinfile/Scores.java
@@ -0,0 +1,27 @@
+/** ****************************************************************************
+ * Scores.java
+ * Kevin Bell
+ *
+ * Objects read from files, calculate & display lowest, highest, & average score
+ **************************************************************************** */
+package studentscoresinfile;
+
+class Scores {
+
+    static double getLowScore(double lowScore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static double getHighScore(double highScore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static double getAverageScore(double averageScore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Scores(double lowScore) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
---
+++ studentScoresInFile/src/studentscoresinfile/StudentScoresInFile.java
@@ -0,0 +1,15 @@
+/** ****************************************************************************
+ * StudentScoresInFile.java
+ * Kevin Bell
+ *
+ * Objects read from files, calculate & display lowest, highest, & average score
+ **************************************************************************** */
+package studentscoresinfile;
+
+import java.io.*;
+
+public class StudentScoresInFile {
+
+    public static void main(String[] args) {
+    } // end main method
+} // end StudentScoresInFile class
