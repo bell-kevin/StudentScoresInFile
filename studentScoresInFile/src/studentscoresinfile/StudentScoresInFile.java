1ab4793891d9970d6272504af4ea856e76bfd4cb
Kevin Bell <bellKevin@pm.me>
2022-09-21 12:22:43 -0600

    Update StudentScoresInFile.java

--- studentScoresInFile/src/studentscoresinfile/StudentScoresInFile.java
+++ studentScoresInFile/src/studentscoresinfile/StudentScoresInFile.java
@@ -11,5 +11,6 @@ import java.io.*;
 public class StudentScoresInFile {
 
     public static void main(String[] args) {
+        System.out.println("Module 3 Student Score by Kevin Bell \n");
     } // end main method
 } // end StudentScoresInFile class
