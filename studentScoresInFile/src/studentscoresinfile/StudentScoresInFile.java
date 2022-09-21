9605c9fd00db724c99b516570c3fc3a61924419a
Kevin Bell <bellKevin@pm.me>
2022-09-21 14:15:47 -0600

    Update StudentScoresInFile.java

--- studentScoresInFile/src/studentscoresinfile/StudentScoresInFile.java
+++ studentScoresInFile/src/studentscoresinfile/StudentScoresInFile.java
@@ -7,10 +7,51 @@
 package studentscoresinfile;
 
 import java.io.*;
+import java.util.*;
 
 public class StudentScoresInFile {
 
     public static void main(String[] args) {
-        System.out.println("Module 3 Student Score by Kevin Bell \n");
+        System.out.println("Chapter 16 Objects in Files by Kevin Bell \n");
+        Scanner computerKeyboardInput = new Scanner(System.in);
+        double score = 0, lowScore = 0, highScore = 0, averageScore = 0;
+        double[] scores = {};
+        String name;
+        String[] names = {};
+        names = new String[5];
+        scores = new double[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter student name: ");
+            name = computerKeyboardInput.next();
+            names[i] = name;
+            System.out.print("Enter test score: ");
+            score = computerKeyboardInput.nextDouble();
+            scores[i] = score;
+        } // end for loop
+        System.out.println("Data written to file");
+        Scores studentList = new Scores(names);
+        String fileName = "studentScores.txt";
+        try (ObjectOutputStream fileOut // write object
+                = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            fileOut.writeObject(studentList);
+            fileOut.reset();
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+        } // end try/catch, and close fileOut automatically
+        try (ObjectInputStream fileIn = new ObjectInputStream( // read object
+                new FileInputStream(fileName))) {
+            while (true) {
+                studentList = (Scores) fileIn.readObject();
+                studentList.display();
+            } // end while loop
+        } catch (EOFException e) { // terminates infinite while loop
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+        } // end try/catch, and close fileOut automatically
+        System.out.println("Low score: " + lowScore);
+        System.out.println("High score: " + highScore);
+        System.out.println("Average score: " + averageScore);
     } // end main method
-} // end StudentScoresInFile class
+} // end Ch16writingAndReadingObjects class
